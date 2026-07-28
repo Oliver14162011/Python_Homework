949d12735fb272bff112679089c9faac56b77552
Oliver16142011 <oliver@waltsgott.de>
2026-07-28 09:04:12 +0200

    Homework

---
+++ arcade.py
@@ -0,0 +1,4 @@
+maxScore = 500
+personalScore = 444
+double_percentage=personalScore/maxScore * 100
+print(double_percentage)
---
+++ operators_testing.py
@@ -0,0 +1,25 @@
+#Operators I
+opZahl1= 3
+opZahl2= 5
+opZahl3= opZahl2-opZahl1
+print(opZahl3)
+opZahl4= 10
+opZahl5= 20
+opZahl6= opZahl4*opZahl5
+print(opZahl6)
+
+#Operators II
+
+erZahl =10
+print(erZahl >= 20) #It will come out "false"
+print(erZahl > 20) #It will come out "false"
+print(erZahl >= 10) #It will come out "true"
+
+#Operators III
+
+aZahl = 6
+print(aZahl < 5 and aZahl < 4) #Both needs to be "true" to get the output "true".
+print(aZahl<7 or aZahl>8) #Now just one of these needs to be "true" to get the output "true"
+print(not(aZahl<9 and aZahl<8)) #If the code we already know is true, the not command turns ist into "false"
+
+
