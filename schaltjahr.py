f008a9a853dab1b9fffea933dced3df7657b3558
Oliver16142011 <oliver@waltsgott.de>
2026-08-02 11:27:51 +0200

    Homework_Update

---
+++ schaltjahr.py
@@ -0,0 +1,10 @@
+year = 1981
+leap_year = False
+if (year % 100 == 0): 
+ if (year % 400 == 0): leap_year = True
+else:
+ if (year % 4 == 0): leap_year = True
+if leap_year:
+ print ("Das Jahr " +str(year) +" ist ein Schaltjahr.")
+else:
+ print ("Das Jahr " +str(year) +" ist kein Schaltjahr.")
