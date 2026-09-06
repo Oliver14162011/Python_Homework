997708d2826404aaf5eec963a430e267ca66df7a
Oliver16142011 <oliver@waltsgott.de>
2026-09-06 10:37:47 +0200

    Update

--- HomeworkListMagicalCreatures.py
+++ HomeworkListMagicalCreatures.py
@@ -25,3 +25,52 @@ for x in sparkle_counts:
  if x  >= 40 and x <= 70:
    print(x) 
 print()
+
+myFloat = 1.0 
+myInt = int(myFloat) # Typecasting  
+myList = [5,3,4,2,1]
+myListAufsteigend = [] 
+
+#Aufsteigend 
+
+for i in range(len(myList)): 
+  if i == 0: 
+   myListAufsteigend.append(myList[0])
+  else:
+   for j in range(len(myListAufsteigend)):
+    if myList[i] < myListAufsteigend[j]: #3 < 5
+     myListAufsteigend.insert(j, myList[i])
+     break 
+   else:
+    myListAufsteigend.append(myList[i])
+    j+1
+  i+1  
+print(myListAufsteigend) 
+#   myListAufsteigend.insert(1,myList) 
+# Absteigend
+myListAbsteigend = []
+
+for i in range(len(myList)): 
+  if i == 0: 
+   myListAbsteigend.append(myList[0])
+  else:
+   for j in range(len(myListAbsteigend)):
+    if myList[i] > myListAbsteigend[j]: #3 < 5
+     myListAbsteigend.insert(j, myList[i])
+     break 
+   else:
+    myListAbsteigend.append(myList[i])
+    j+1
+  i+1  
+print(myListAbsteigend) 
+
+
+
+
+
+
+
+
+# Ausgangsituaton myListAufsteigend ist leer.
+# Erster Durchlauf = Index 0  myListAufsteigend wir befüllt  
+# Zweiter Durchlauf = Index 1  Wenn myList[i] > myListAufsteigend[i-1]
